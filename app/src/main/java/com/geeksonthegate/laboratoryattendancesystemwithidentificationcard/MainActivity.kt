10640a0770db23f9c37af521c436f21d6c0d5b2c
RomancingTK <--global>
2018-08-16 16:02:19 +0900

    実装: メイン画面からカード読み取り待機画面への遷移

--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/MainActivity.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.geeksonthegate.laboratoryattendancesystemwithidentificationcard
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val readIdButton: Button = findViewById(R.id.read_id) as Button
+        readIdButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                val intent: Intent = Intent(this@MainActivity, IdWatingActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
