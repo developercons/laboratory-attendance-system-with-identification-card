c6dc47a36e827d234d744624cc3ac8ebc9f7fb0e
RomancingTK <al15026@shibaura-it.ac.jp>
2018-08-21 15:10:08 +0900

    修正: DBのIDmにByteArrayが使用できないため, Stringへ変更

--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/AttendanceLog.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/AttendanceLog.kt
@@ -1,9 +1,10 @@
 package com.geeksonthegate.laboratoryattendancesystemwithidentificationcard
 
+import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
 
 open class AttendanceLog(@PrimaryKey var logID: String = UUID.randomUUID().toString(),
-                         var IDm: ByteArray? = null,
+                         var IDm: String? = null,
                          var enterTime: Date? = null,
-                         var exitTime: Date? = null)
+                         var exitTime: Date? = null) : RealmObject()
--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/Student.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/Student.kt
@@ -3,7 +3,7 @@ package com.geeksonthegate.laboratoryattendancesystemwithidentificationcard
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-open class Student(@PrimaryKey var IDm: ByteArray? = null,
+open class Student(@PrimaryKey var IDm: String? = null,
                    var studentID: String? = null,
                    var name: String? = null,
                    var lab: Lab? = null) : RealmObject()
