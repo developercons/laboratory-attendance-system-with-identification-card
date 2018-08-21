3ed9eab630f676d932b0642ab9f9aef3b88b8759
RomancingTK <al15026@shibaura-it.ac.jp>
2018-08-21 15:39:02 +0900

    修正: 'ID'をキャメルケースへ

--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/AttendanceLog.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/AttendanceLog.kt
@@ -4,7 +4,7 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
 
-open class AttendanceLog(@PrimaryKey var logID: String = UUID.randomUUID().toString(),
-                         var IDm: String? = null,
+open class AttendanceLog(@PrimaryKey var logId: String = UUID.randomUUID().toString(),
+                         var idm: String? = null,
                          var enterTime: Date? = null,
                          var exitTime: Date? = null) : RealmObject()
--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/Student.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/Student.kt
@@ -3,7 +3,7 @@ package com.geeksonthegate.laboratoryattendancesystemwithidentificationcard
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-open class Student(@PrimaryKey var IDm: String? = null,
-                   var studentID: String? = null,
+open class Student(@PrimaryKey var idm: String? = null,
+                   var studentId: String? = null,
                    var name: String? = null,
                    var lab: Lab? = null) : RealmObject()
