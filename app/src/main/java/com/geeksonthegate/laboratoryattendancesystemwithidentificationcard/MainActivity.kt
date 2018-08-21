3f7d3698e680eb4b86d0187af8028c9dcd23baaa
RomancingTK <al15026@shibaura-it.ac.jp>
2018-08-21 13:57:39 +0900

    追加: メイン画面でテスト用ダミーデータを追加する処理

--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/MainActivity.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/MainActivity.kt
@@ -4,17 +4,32 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import io.realm.Realm
+import io.realm.RealmList
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var mRealm: Realm? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mRealm = Realm.getDefaultInstance()
 
-        enter.setOnClickListener{ scanStudentcartButtonTapped(it) }
-        exit.setOnClickListener{ scanStudentcartButtonTapped(it)}
+        enter.setOnClickListener { scanStudentcartButtonTapped(it) }
+        exit.setOnClickListener { scanStudentcartButtonTapped(it) }
 
+        // 以降、テスト用ダミーデータの登録
+        val coreTimeArray = RealmList<CoreTime>()
+        for (i in 0..6) {
+            coreTimeArray.add(CoreTime(i, GregorianCalendar(2000, 0, 1, 11, 0).time,
+                    GregorianCalendar(2000, 0, 1, 11, 0).time, false))
+        }
+        val lab = Lab(labName = "福田研究室", coreTimeArray = coreTimeArray)
+        mRealm?.executeTransaction { it.copyToRealm(lab) }
+        Toast.makeText(this, "福田研究室を追加しました", Toast.LENGTH_SHORT).show()
     }
 
     // カード読み取り(入退室)画面に遷移
