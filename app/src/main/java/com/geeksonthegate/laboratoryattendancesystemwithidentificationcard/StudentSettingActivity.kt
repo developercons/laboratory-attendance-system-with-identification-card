e9a36e1591bdcac7dc896a6e5f730c3c0f3c6730
RomancingTK <al15026@shibaura-it.ac.jp>
2018-08-22 21:05:20 +0900

    追加: リスト表示するセルレイアウト

--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/StudentSettingActivity.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/StudentSettingActivity.kt
@@ -2,11 +2,35 @@ package com.geeksonthegate.laboratoryattendancesystemwithidentificationcard
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.geeksonthegate.laboratoryattendancesystemwithidentificationcard.adapter.CoreTimeRealmAdapter
+import com.geeksonthegate.laboratoryattendancesystemwithidentificationcard.model.CoreTime
+import com.geeksonthegate.laboratoryattendancesystemwithidentificationcard.model.Student
+import io.realm.RealmList
+import kotlinx.android.synthetic.main.activity_student_setting.*
+import java.util.*
 
 class StudentSettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_setting)
+/*
+        val scanLabel = intent.getStringExtra("scan_label")
+        val idm = intent.getStringExtra("idm")
+        var coreTimeArray = RealmList<CoreTime>()
+        for (i in 0..6) {
+            coreTimeArray.add(CoreTime(GregorianCalendar(2000, 0, 1, 11, 0).time,
+                    GregorianCalendar(2000, 0, 1, 11, 0).time, false))
+        }
+        if (scanLabel.compareTo(getString(R.string.edit))) {
+            val student: Student = intent.getSerializableExtra("student") as Student
+            nameBox.setText(student.name)
+            studentIdBox.setText(student.studentId)
+            // TODO: spinnerの選択肢を動的に設定する機能
+            coreTimeArray = student.lab?.coreTimeArray ?: throw NullPointerException("Error")
+        }
+        val adapter = CoreTimeRealmAdapter(coreTimeArray)
+        coreTimeList.adapter = adapter
+*/
     }
 }
