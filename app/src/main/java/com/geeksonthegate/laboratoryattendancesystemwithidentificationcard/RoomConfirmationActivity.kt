b53b3786a04bc777ec809f4bdc799d3bf7503ee7
ShoFurukawa <al15082@shibaura-it.ac.jp>
2018-08-23 16:43:41 +0900

    入退室確認画面にコアタイム以外の情報を表示

--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/RoomConfirmationActivity.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/RoomConfirmationActivity.kt
@@ -1,5 +1,6 @@
 package com.geeksonthegate.laboratoryattendancesystemwithidentificationcard
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -7,7 +8,8 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
-import com.geeksonthegate.laboratoryattendancesystemwithidentificationcard.R.id.textView8
+import com.geeksonthegate.laboratoryattendancesystemwithidentificationcard.R.id.*
+import com.geeksonthegate.laboratoryattendancesystemwithidentificationcard.model.Lab
 import com.geeksonthegate.laboratoryattendancesystemwithidentificationcard.model.Student
 import io.realm.Realm
 import io.realm.kotlin.where
@@ -16,6 +18,7 @@ import java.util.*
 
 class RoomConfirmationActivity : AppCompatActivity() {
     private lateinit var realm: Realm
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room_confirmation)
@@ -30,16 +33,31 @@ class RoomConfirmationActivity : AppCompatActivity() {
                 setTitle(R.string.exit)
             }
         }
-        val idm:ByteArray = intent.getByteArrayExtra("idm")
-        Toast.makeText(this,Arrays.toString(idm),Toast.LENGTH_SHORT).show()
-        val scanStudent = realm.where<Student>().contains("idm", Arrays.toString(idm)).findFirst()
+        val idm: ByteArray = intent.getByteArrayExtra("idm")
+        //Toast.makeText(this,Arrays.toString(idm),Toast.LENGTH_SHORT).show()
+        val student = realm.where<Student>().contains("idm", Arrays.toString(idm)).findFirst()
+        //val student: Student? = scanStudent
+        val lab: String? = student?.lab?.labName
+        val name: String? = student?.name
+        val studentId: String? = student?.studentId
+
+        //Toast.makeText(this, student.toString(), Toast.LENGTH_SHORT).show()
         val cal = Calendar.getInstance()
         val hour: Int = cal.get(Calendar.HOUR_OF_DAY)
         val minutes: Int = cal.get(Calendar.MINUTE)
         //Toast.makeText(this,minutes.toString(),Toast.LENGTH_SHORT).show()
-        val textView: TextView = findViewById(textView8)
-        textView.text = scanStudent.toString()
-        //textView.text = hour.toString() + ":" + minutes.toString()
+
+        val lab_view: TextView = findViewById(R.id.lab)
+        val name_view: TextView = findViewById(R.id.name)
+        val studentId_view: TextView = findViewById(R.id.studentId)
+        val currentTime_view: TextView = findViewById(R.id.currentTime)
+
+        lab_view.text=lab.toString()
+        name_view.text=name.toString()
+        studentId_view.text=studentId.toString()
+       // lab_view.text=lab.toString()
+        currentTime_view.text = hour.toString() + ":" + minutes.toString()
+        //textView.text = scanStudent.toString()
         //Toast.makeText(this,  title, Toast.LENGTH_SHORT).show()
     }
 
@@ -59,4 +77,3 @@ class RoomConfirmationActivity : AppCompatActivity() {
         realm.close()
     }
 }
-
