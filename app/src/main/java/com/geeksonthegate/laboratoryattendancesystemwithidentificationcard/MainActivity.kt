89f0bb412c55cc86365caff60b4eec62c22e0533
RomancingTK <al15026@shibaura-it.ac.jp>
2018-08-23 18:35:24 +0900

    追加: ダミーデータの復活

--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/MainActivity.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/MainActivity.kt
@@ -22,6 +22,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         realm = Realm.getDefaultInstance()
 
+        // 以降、テスト用ダミーデータの登録
+        // 研究室
+        val coretimeArray = RealmList<CoreTime>()
+        for (i in 0..6) {
+            coretimeArray.add(CoreTime(GregorianCalendar(2000, 0, 1, 11, 0).time,
+                    GregorianCalendar(2000, 0, 1, 11, 0).time, false))
+        }
+        val labArray = mutableListOf<Lab>()
+        try {
+            labArray.add(Lab(labName = "福田研究室", coretimeArray = coretimeArray))
+            labArray.add(Lab(labName = "菅谷研究室", coretimeArray = coretimeArray))
+            labArray.add(Lab(labName = "宇佐美研究室", coretimeArray = coretimeArray))
+            labArray.add(Lab(labName = "平川研究室", coretimeArray = coretimeArray))
+        } catch (exception: Exception) {
+            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+        } finally {
+            if (labArray.size != 0) {
+                realm?.executeTransaction { for (item in labArray) it.insertOrUpdate(item) }
+            }
+        }
+
+        // 学生
+        val studentArray = mutableListOf<Student>()
+        try {
+            studentArray.add(Student(listOf<Byte>(1, 16, 14, 0, 60, 20, -31, 0).toString(), "AL15026", "岸本太郎", Lab(labName = "菅谷研究室", coretimeArray = coretimeArray)))
+            studentArray.add(Student(listOf<Byte>(1, 16, 8, 0, 60, 20, -99, 1).toString(), "AL15082", "迫真古川先輩", Lab(labName = "平川研究室", coretimeArray = coretimeArray)))
+            studentArray.add(Student(listOf<Byte>(1, 18, 3, 18, -45, 24, 34, 28).toString(), "AL15065", "Gitの鬼", Lab(labName = "福田研究室", coretimeArray = coretimeArray)))
+        } catch (exception: Exception) {
+            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+        } finally {
+            if (studentArray.size != 0) {
+                realm?.executeTransaction { for (item in studentArray) it.insertOrUpdate(item) }
+            }
+        }
+        realm?.executeTransaction { for (item in studentArray) it.insertOrUpdate(item) }
+
+        // 入退出ログ
+        val logArray = mutableListOf<AttendanceLog>()
+        logArray.add(AttendanceLog(listOf<Byte>(1, 16, 8, 0, 60, 20, -99, 1).toString(), GregorianCalendar(2018, 8, 20, 11, 0).time, GregorianCalendar(2018, 8, 20, 18, 0).time))
+        logArray.add(AttendanceLog(listOf<Byte>(1, 16, 8, 0, 60, 20, -99, 1).toString(), GregorianCalendar(2018, 8, 20, 11, 0).time, GregorianCalendar(2018, 8, 20, 18, 0).time))
+        logArray.add(AttendanceLog(listOf<Byte>(1, 18, 3, 18, -45, 24, 34, 28).toString(), GregorianCalendar(2018, 8, 20, 11, 0).time, GregorianCalendar(2018, 8, 20, 18, 0).time))
+        realm?.executeTransaction { for (item in logArray) it.insertOrUpdate(item) }
+
         enter.setOnClickListener { scanStudentcardButtonTapped(it) }
         exit.setOnClickListener { scanStudentcardButtonTapped(it) }
         register.setOnClickListener { scanStudentcardButtonTapped(it) }
