b6e4b05dd338555c9a08ca33e748038787674ea8
ShoFurukawa <al15082@shibaura-it.ac.jp>
2018-08-23 12:56:49 +0900

    入退室確認画面に時間を表示

--- app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/RoomConfirmationActivity.kt
+++ app/src/main/java/com/geeksonthegate/laboratoryattendancesystemwithidentificationcard/RoomConfirmationActivity.kt
@@ -4,6 +4,12 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.geeksonthegate.laboratoryattendancesystemwithidentificationcard.R.id.textView8
+import kotlinx.android.synthetic.*
+import java.util.*
 
 class RoomConfirmationActivity : AppCompatActivity() {
 
@@ -13,13 +19,20 @@ class RoomConfirmationActivity : AppCompatActivity() {
 
         val id = intent.getStringExtra("scan_label")
         when (id) {
-            getString(R.string.enter)-> {setTitle(R.string.enter)}
-            getString(R.string.exit)-> {setTitle(R.string.exit)}
+            getString(R.string.enter) -> {
+                setTitle(R.string.enter)
+            }
+            getString(R.string.exit) -> {
+                setTitle(R.string.exit)
+            }
         }
+        
         val cal = Calendar.getInstance()
-        val hour:Int = cal.get(Calendar.HOUR_OF_DAY)
-        val minutes:Int = cal.get(Calendar.MINUTE)
-        Toast.makeText(this,hour.toString(),Toast.LENGTH_SHORT).show()
+        val hour: Int = cal.get(Calendar.HOUR_OF_DAY)
+        val minutes: Int = cal.get(Calendar.MINUTE)
+        //Toast.makeText(this,minutes.toString(),Toast.LENGTH_SHORT).show()
+        val textView: TextView = findViewById(R.id.textView8)
+        textView.text = hour.toString() + ":" + minutes.toString()
         //Toast.makeText(this,  title, Toast.LENGTH_SHORT).show()
     }
 
